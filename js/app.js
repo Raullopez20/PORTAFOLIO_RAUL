c3fc913d28bdaf74603e617383594f1ae300e862
Raullopez20 <149530497+Raullopez20@users.noreply.github.com>
2024-11-22 20:10:46 +0100

    pr5
    pr5

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     feedback.textContent = "Enviando mensaje...";
 
     try {
-      const response = await fetch(".netlify/v1/functions/send-email.js", {
+      const response = await fetch("/.netlify/functions/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
